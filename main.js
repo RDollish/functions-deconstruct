db209bcd809382797d0eebb9c1fd39e0dfa195bb
Renee Doll <renee@reneedoll.com>
2022-10-07 10:08:44 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const A3paper = () => {
+    console.log("Get a piece of A3 paper")
+}
+
+const foldinHalf = () => {
+    console.log("Fold it in half")
+}
+
+const cleanFountainPen = () => {
+    console.log("Clean my fountain pen")
+}
+
+const inkpenFill = () => {
+    console.log("Fill fountain pen with ink color I want")
+}
+
+const writeLetter = () => {
+    console.log("Write letter")
+}
+
+const foldForEnvelope = () => {
+    console.log("Fold letter to fit in envelope")
+}
+
+const meltWaxColor = () => {
+    console.log("Melt wax color for seal")
+}
+
+const pressWaxSeal = () => {
+    console.log("Press seal into wax")
+}
+const addressEnvelope = () => {
+    console.log("Address the envelope")
+}
+
+const addStamp = () => {
+    console.log("Add stamp to envelope")
+}
+
+const mailLetter = () => {
+    console.log("Mail letter")
+}
+
+A3paper()
+foldinHalf()
+cleanFountainPen()
+inkpenFill()
+writeLetter()
+foldForEnvelope()
+meltWaxColor()
+pressWaxSeal()
+addressEnvelope()
+addStamp()
+mailLetter()
